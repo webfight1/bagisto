e2af122f1f852de903bc4631281a1b191568ed98
mrvx <veiko.teekel@gmail.com>
2026-02-07 14:08:59 +0200

    aaa

--- routes/api.php
+++ routes/api.php
@@ -27,15 +27,7 @@ Route::get('/v1/product/{slug}', [SingleProductController::class, 'show']);
 // Category products by slug
 Route::get('/v1/category/{slug}', [CategoryProductController::class, 'index']);
 
-Route::post('/payments/esto/webhook', function(\Illuminate\Http\Request $request) {
-    \Illuminate\Support\Facades\Log::info('ESTO webhook received', [
-        'headers' => $request->headers->all(),
-        'body' => $request->all(),
-        'raw' => $request->getContent(),
-    ]);
-    
-    return response()->json(['message' => 'Webhook endpoint works', 'received' => $request->all()]);
-})
+Route::post('/payments/esto/webhook', [\App\Http\Controllers\Api\EstoWebhookController::class, 'handle'])
     ->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class)
     ->name('esto.webhook');
 
