474d104cd6fe56668d3be3d54a82c8bc747d0a81
mrvx <veiko.teekel@gmail.com>
2026-02-13 11:30:59 +0200

    veel tõlge

--- packages/Webkul/Admin/src/Resources/views/emails/orders/created.blade.php
+++ packages/Webkul/Admin/src/Resources/views/emails/orders/created.blade.php
@@ -24,9 +24,25 @@
     <div style="display: flex;flex-direction: row;margin-top: 20px;justify-content: space-between;margin-bottom: 40px;">
         @if ($order->shipping_address)
             <div style="line-height: 25px;">
-                
+                <div style="font-size: 16px;font-weight: 600;color: #121A26;">
+                    @lang('admin::app.emails.orders.shipping-address')
+                </div>
 
-                
+                <div style="font-size: 16px;font-weight: 400;color: #384860;margin-bottom: 40px;">
+                    {{ $order->shipping_address->company_name ?? '' }}<br/>
+
+                    {{ $order->shipping_address->name }}<br/>
+                    
+                    {{ $order->shipping_address->address }}<br/>
+                    
+                    {{ $order->shipping_address->postcode . " " . $order->shipping_address->city }}<br/>
+                    
+                    {{ $order->shipping_address->state }}<br/>
+
+                    ---<br/>
+
+                    @lang('admin::app.emails.orders.contact') : {{ $order->billing_address->phone }}
+                </div>
 
                 <div style="font-size: 16px;font-weight: 600;color: #121A26;">
                     @lang('admin::app.emails.orders.shipping')
@@ -40,7 +56,25 @@
 
         @if ($order->billing_address)
             <div style="line-height: 25px;">
-              
+                <div style="font-size: 16px;font-weight: 600;color: #121A26;">
+                    @lang('admin::app.emails.orders.billing-address')
+                </div>
+
+                <div style="font-size: 16px;font-weight: 400;color: #384860;margin-bottom: 40px;">
+                    {{ $order->billing_address->company_name ?? '' }}<br/>
+
+                    {{ $order->billing_address->name }}<br/>
+                    
+                    {{ $order->billing_address->address }}<br/>
+                    
+                    {{ $order->billing_address->postcode . " " . $order->billing_address->city }}<br/>
+                    
+                    {{ $order->billing_address->state }}<br/>
+
+                    ---<br/>
+
+                    @lang('admin::app.emails.orders.contact') : {{ $order->billing_address->phone }}
+                </div>
 
                 <div style="font-size: 16px;font-weight: 600;color: #121A26;">
                     @lang('admin::app.emails.orders.payment')
@@ -67,7 +101,7 @@
         border-spacing: 0;width: 100%">
             <thead>
                 <tr style="color: #121A26;border-top: 1px solid #CBD5E1;border-bottom: 1px solid #CBD5E1;">
-                    @foreach (['name', 'price', 'qty'] as $item)
+                    @foreach (['sku', 'name', 'price', 'qty'] as $item)
                         <th style="text-align: left;padding: 15px">
                             @lang('admin::app.emails.orders.' . $item)
                         </th>
@@ -78,33 +112,20 @@
             <tbody style="font-size: 16px;font-weight: 400;color: #384860;">
                 @foreach ($order->items as $item)
                     <tr style="vertical-align: text-top;">
-                      
+                        <td style="text-align: left;padding: 15px">
+                            {{ $item->getTypeInstance()->getOrderedItem($item)->sku }}
+                        </td>
 
                         <td style="text-align: left;padding: 15px">
                             {{ $item->name }}
 
                             @if (isset($item->additional['attributes']))
                                 <div>
+
                                     @foreach ($item->additional['attributes'] as $attribute)
-                                        @if (
-                                            ! isset($attribute['attribute_type'])
-                                            || $attribute['attribute_type'] !== 'file'
-                                        )
-                                            <b>{{ $attribute['attribute_name'] }} : </b>{{ $attribute['option_label'] }}<br>
-                                        @else
-                                            <b>{{ $attribute['attribute_name'] }} : </b>
-
-                                            <a
-                                                href="{{ Storage::url($attribute['option_label']) }}"
-                                                class="text-blue-600 hover:underline"
-                                                download="{{ File::basename($attribute['option_label']) }}"
-                                            >
-                                                {{ File::basename($attribute['option_label']) }}
-                                            </a>
-
-                                            <br>
-                                        @endif
+                                        <b>{{ $attribute['attribute_name'] }} : </b>{{ $attribute['option_label'] }}</br>
                                     @endforeach
+
                                 </div>
                             @endif
                         </td>
@@ -200,7 +221,7 @@
                         {{ core()->formatBasePrice($order->base_shipping_amount) }}
                     </span>
                 </div>
-
+                
                 <div style="display: grid;gap: 20px;grid-template-columns: repeat(2, minmax(0, 1fr));">
                     <span>
                         @lang('admin::app.emails.orders.shipping-handling-incl-tax')
