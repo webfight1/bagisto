29259252208ba0fda2992f5b48074f44cb143715
mrvx <veiko.teekel@gmail.com>
2026-04-27 15:20:51 +0300

    Add parcel-locker log channel

--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'parcel-locker' => [
+            'driver'               => 'daily',
+            'path'                 => storage_path('logs/parcel-locker.log'),
+            'level'                => env('LOG_LEVEL', 'debug'),
+            'days'                 => 30,
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver'               => 'slack',
             'url'                  => env('LOG_SLACK_WEBHOOK_URL'),
